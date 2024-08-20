7422c60d270a0fadf7b348a9379de2398d73b066
VladFigy <vladimirkupryukhin@gmail.com>
2024-08-20 16:11:29 -0700

    Initial stuff

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+
+import NavBar from '../src/views/NavBar.vue'
+</script>
+
+<template>
+
+  <div class="navbar">
+    <NavBar></NavBar>
+
+  </div>
+
+  <RouterView />
+  
+
+</template>
+
+
+<style lang="scss">
+
+.navbar{
+  display: flex;
+}
+
+
+
+
+
+</style>
+
---
+++ src/views/NavBar.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+
+</script>
+
+
+<template>
+    <div class="test" style="border: 2px solid blue">
+        <nav>
+            <RouterLink class="router-button" to="/">Home</RouterLink>
+            <RouterLink class="router-button" to="/about">About</RouterLink>
+        </nav>
+
+    </div>
+
+
+
+</template>
+
+<style>
+
+
+.router-button{
+    background-color: rgb(212, 28, 212);
+    margin: 5px;
+}
+
+.button-container{
+    margin: 5px;
+}
+
+.test{
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+</style>
+
+
